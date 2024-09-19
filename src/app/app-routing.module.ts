caaa045a34de389e7e1cc431c9b74f3ea0a09314
Vinicio Chavarria <vichavarria21@gmaail.com>
2024-09-19 07:44:38 -0600

    Proyecto de Angular, se conecta a un backend para hacer uso de funcionamiento de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { isAuthenticatedGuard } from './auth/guards/is-authenticated.guard';
+import { isNotAthenticadGuard } from './auth/guards';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    // guards
+    canActivate: [isNotAthenticadGuard],
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    // guards
+    canActivate: [isAuthenticatedGuard],
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject( FormBuilder );
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]]
+  });
+
+  register() {
+    const {name, email, password} = this.myForm.value;
+    this.authService.register( name, email, password )
+    .subscribe({
+      next: () => this.router.navigateByUrl('/dashboard'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error' );
+      }
+    })
+  }
+
+}
